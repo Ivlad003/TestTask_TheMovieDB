0e66124f292f052616b5d7b57564c6d72181db8b
Vlad Kosmach <ivlad003@gmail.com>
2021-08-29 23:35:55 +0300

    Initial Commit

---
+++ TestTask_TheMovieDB/TestTask_TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask_TheMovieDBApp.swift
+//  TestTask_TheMovieDB
+//
+//  Created by kosmodev on 29.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestTask_TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
